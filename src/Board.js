4062112ee7d499623a0fcb46f168600819812ac8
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-07-10 06:29:48 +0100

    added cards on the board

--- src/Board.js
+++ src/Board.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Card from "./Card";
 
 const Board = ({ points }) => {
   return (
     <div className="board">
       <h2>Selected Points:</h2>
       {points.map((point, index) => (
-        <p key={index}>{point}</p>
+        // <p key={index}>{point}</p>;
+        <div className="board_card">
+          <Card key={index}>{point}</Card>
+        </div>
       ))}
     </div>
   );
