cc51433e38517bc7678ca6eac6c6d8a31d7673da
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-07-14 08:39:43 +0100

    button adjustments

--- src/Display-button.js
+++ src/Display-button.js
@@ -3,7 +3,17 @@ import Button from "@mui/material/Button";
 import { useState } from "react";
 
 const DisplayButton = () => {
-  return <Button variant="contained">Display</Button>;
+  const [display, setDisplay] = useState(false);
+
+  return (
+    <Button
+      variant="contained"
+      className="display-button"
+      style={{ marginTop: "45px", backgroundColor: "#011d55" }}
+    >
+      Display
+    </Button>
+  );
 };
 
 export default DisplayButton;
