ab47cb3cf1243262338c1cf55fdcb28a020868dc
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-08-26 09:01:33 +0100

    Installed Vercel

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const WebSocket = require("ws");
 
 // Set up the WebSocket server on port 8080
-const wss = new WebSocket.Server({ port: 8080 });
+const PORT = process.env.PORT || 8080;
+const wss = new WebSocket.Server({ port: PORT });
 
 wss.on("connection", (ws) => {
   console.log("A new client connected.");
